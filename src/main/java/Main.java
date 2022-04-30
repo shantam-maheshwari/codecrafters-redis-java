7543ebdcace27a76b217f915cdda47a8ffcb7e5f
Shantam Maheshwari <shantam.maheshwari@learner.manipal.edu>
2022-04-30 22:07:43 +0530

    Implement the SET & GET commands: handle set and get commands; implement data store using hashmap

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,13 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
   public static void main(String[] args) {
+    Map<String, String> dataStore = new HashMap<>();
+
     int port = 6379;
     ServerSocket serverSocket = null;
     Socket clientSocket = null;
@@ -16,7 +20,7 @@ public class Main {
       serverSocket = new ServerSocket(port);
       while (true) {
         clientSocket = serverSocket.accept();
-        thread = new ClientHandlerThread(clientSocket);
+        thread = new ClientHandlerThread(clientSocket, dataStore);
         thread.start();
       }
     } catch (IOException e) {
@@ -35,9 +39,11 @@ public class Main {
 
 class ClientHandlerThread extends Thread {
   private Socket clientSocket;
+  Map<String, String> dataStore;
 
-  public ClientHandlerThread(Socket clientSocket) {
+  public ClientHandlerThread(Socket clientSocket, Map<String, String> dataStore) {
     this.clientSocket = clientSocket;
+    this.dataStore = dataStore;
   }
 
   public void run() {
@@ -48,12 +54,16 @@ class ClientHandlerThread extends Thread {
     String requestBulkString;
     int requestBulkStringLength;
 
-    // PING
+    // PING, SET
     String responseSimpleString;
 
-    // ECHO
+    // ECHO, GET
     String responseBulkString;
     int responseBulkStringLength;
+
+    // SET, GET
+    String key;
+    String value;
 
     try {
       in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
@@ -103,6 +113,71 @@ class ClientHandlerThread extends Thread {
             // send bulk string
             responseBulkStringLength = requestBulkStringLength;
             responseBulkString = "$" + responseBulkStringLength + "\r\n" + requestBulkString + "\r\n";
+            out.print(responseBulkString);
+            out.flush();
+          }
+
+          // SET
+          else if (requestBulkString.equals("set")) {
+            // get length of bulk string
+            requestBulkString = in.readLine();
+            if (requestBulkString == null) {
+              break;
+            }
+            requestBulkStringLength = Integer.parseInt(requestBulkString.substring(1));
+
+            // get bulk string (key)
+            requestBulkString = in.readLine();
+            if (requestBulkString == null) {
+              break;
+            }
+            key = requestBulkString;
+
+            // get length of bulk string
+            requestBulkString = in.readLine();
+            if (requestBulkString == null) {
+              break;
+            }
+            requestBulkStringLength = Integer.parseInt(requestBulkString.substring(1));
+
+            // get bulk string (value)
+            requestBulkString = in.readLine();
+            if (requestBulkString == null) {
+              break;
+            }
+            value = requestBulkString;
+
+            // set key, value
+            dataStore.put(key, value);
+
+            // send simple string
+            responseSimpleString = "+OK\r\n";
+            out.print(responseSimpleString);
+            out.flush();
+          }
+
+          // GET
+          else if (requestBulkString.equals("get")) {
+            // get length of bulk string
+            requestBulkString = in.readLine();
+            if (requestBulkString == null) {
+              break;
+            }
+            requestBulkStringLength = Integer.parseInt(requestBulkString.substring(1));
+
+            // get bulk string (key)
+            requestBulkString = in.readLine();
+            if (requestBulkString == null) {
+              break;
+            }
+            key = requestBulkString;
+
+            // get value
+            value = dataStore.getOrDefault(key, "(nil)");
+
+            // send bulk string
+            responseBulkStringLength = value.length();
+            responseBulkString = "$" + responseBulkStringLength + "\r\n" + value + "\r\n";
             out.print(responseBulkString);
             out.flush();
           }
